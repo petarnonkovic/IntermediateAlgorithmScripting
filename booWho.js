c8fd7eee3c0567a676dedcd2c43ae2eba7e8cd5e
unknown <tekumseh777@gmail.com>
2017-08-05 13:39:26 +0200

    Boo Who Challenge Solution

---
+++ booWho.js
@@ -0,0 +1,14 @@
+
+function booWho(bool) {
+  if (typeof bool === 'boolean') {
+    return true;
+  }
+  return false;
+}
+
+console.log(booWho(null));
+console.log(booWho(true));
+console.log(booWho(false));
+console.log(booWho([1, 2, 3]));
+console.log(booWho([].slice));
+console.log(booWho({ "a": 1 }));
